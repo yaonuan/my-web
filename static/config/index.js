0f1629aea618fad697de9645ad659fa91932c080
yaonuan <806039077@qq.com>
2018-09-10 15:33:53 +0800

    modify index.js

--- static/config/index.js
+++ static/config/index.js
@@ -6,14 +6,14 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  // window.SITE_CONFIG['baseUrl'] = 'http://115.233.227.46:18899';
-  window.SITE_CONFIG['baseUrl'] = 'http://127.0.0.1:8899';
+  window.SITE_CONFIG['baseUrl'] = 'http://115.233.227.46:18899';
+  // window.SITE_CONFIG['baseUrl'] = 'http://127.0.0.1:8899';
   // window.SITE_CONFIG['baseUrl'] = 'http://106.14.176.127:18899';
   
 
   // 嵌套iframe地址
-  // window.SITE_CONFIG['nestIframeUrl'] = 'http://115.233.227.46:18899';
-  window.SITE_CONFIG['nestIframeUrl'] = 'http://127.0.0.1:8899';
+  window.SITE_CONFIG['nestIframeUrl'] = 'http://115.233.227.46:18899';
+  // window.SITE_CONFIG['nestIframeUrl'] = 'http://127.0.0.1:8899';
   // window.SITE_CONFIG['nestIframeUrl'] = 'http://106.14.176.127:18899';
 
   // 嵌套iframe路由名称列表
